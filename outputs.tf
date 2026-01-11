e40fd8d052064f6375b010f988916729b685f76d
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:40:03 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_factory_linked_service_synapses" {
+  description = "All data_factory_linked_service_synapse resources"
+  value       = azurerm_data_factory_linked_service_synapse.data_factory_linked_service_synapses
+}
+
